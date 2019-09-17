b178ebc56a5b0ecc16ddd1bbac68e3cd1f3d3f54
diouf <thirtsoft@gmail.com>
2019-09-17 14:18:44 +0000

    Mise en place complète de l'interface d'accueil et intégration complète de Bootstrap

---
+++ onlineshopping/src/main/webapp/assets/js/myapp.js
@@ -0,0 +1,17 @@
+$(function() {
+	// solving the active menu problem
+	switch(menu) {
+	
+	case 'About Us':
+		$('#about').addClass('active');
+		break;
+	case 'Contact Us':
+		$('#contact').addClass('active');
+		break;
+	case 'Home':
+		$('#home').addClass('active');
+		break;
+		
+	}
+	
+});
